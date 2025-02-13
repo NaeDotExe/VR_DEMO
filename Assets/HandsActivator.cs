3da887129a57718c1f2cf8299e0dc5ebe71cc3af
Hannaé <hannaedelassus@gmail.com>
2025-02-13 11:39:00 +0100

    [fix] hands not showing

--- Assets/HandsActivator.cs
+++ Assets/HandsActivator.cs
@@ -1,7 +1,11 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Hands;
+using UnityEngine.SubsystemsImplementation;
+using UnityEngine.XR.Management;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.Hands.OpenXR;
 
 public class HandsActivator : MonoBehaviour
 {
@@ -9,19 +13,69 @@ public class HandsActivator : MonoBehaviour
 
     void Start()
     {
-        List<XRHandSubsystem> subsystems = new List<XRHandSubsystem>();
-        SubsystemManager.GetSubsystems(subsystems);
+        //ForceHandActivation();
+
+        //OpenXRSettings settings = OpenXRSettings.Instance;
+        //if (settings != null)
+        //{
+        //    Debug.Log("OpenXR Settings found, enabling Hand Tracking...");
+        //    settings.GetFeature<HandTracking>().enabled = true;
+        //}
+        //else
+        //{
+        //    Debug.LogError("OpenXR Settings not found!");
+        //}
+
+        CheckSubsytems();
+    }
+
+   private void CheckSubsytems()
+    {
+        var subsystems = new List<ISubsystem>();
+        SubsystemManager.GetInstances(subsystems);
+
+        Debug.Log("test");
 
         if (subsystems.Count > 0)
         {
-            handSubsystem = subsystems[0];
-            handSubsystem.Start(); 
-            Debug.Log("XRHandSubsystem ok !");
+            foreach (var subsystem in subsystems)
+            {
+                Debug.Log($"Sous-système détecté : {subsystem.GetType().FullName}");
+            }
         }
         else
         {
-            Debug.LogError("no subsystem.");
+            Debug.LogError("Aucun sous-système détecté !");
         }
+    }
+
+    private void ForceHandActivation()
+    {
+        handSubsystem = XRGeneralSettings.Instance.Manager.activeLoader.GetLoadedSubsystem<XRHandSubsystem>();
+
+        Debug.Log("aaaaaaaa");
+
+        if (handSubsystem != null)
+        {
+            handSubsystem.Start();
+            Debug.Log("✅ XR Hand Tracking activé !");
+        }
+        else
+        {
+            Debug.LogError("❌ Impossible d’activer XR Hand Tracking !");
+        }
+    }
+
+    private void Update()
+    {
+        if (handSubsystem == null)
+        {
+            List<XRHandSubsystem> list = new List<XRHandSubsystem>();
+            SubsystemManager.GetSubsystems(list);
+            handSubsystem = list[0];
+        }
+
+        Debug.LogFormat("right hand tracked ? {0}", handSubsystem.rightHand.isTracked);
     }
 }
 
